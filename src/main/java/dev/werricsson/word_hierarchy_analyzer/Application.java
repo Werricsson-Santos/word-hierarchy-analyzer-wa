e2c2ad5b21bf47c0bc6418ae790ec07edfba3502
Werricsson <werricsson.santos@hotmail.com>
2024-09-01 08:03:39 -0300

    :tada: Init project

---
+++ src/main/java/dev/werricsson/word_hierarchy_analyzer/Application.java
@@ -0,0 +1,14 @@
+package dev.werricsson.word_hierarchy_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/werricsson/word_hierarchy_analyzer/WordHierarchyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.werricsson.word_hierarchy_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordHierarchyAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
