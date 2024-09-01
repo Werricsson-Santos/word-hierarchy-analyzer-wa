24fb99d67a573a4bb14bbc50827d747a29cfff3a
Werricsson <werricsson.santos@hotmail.com>
2024-09-01 17:29:36 -0300

    :construction: Creating HierarchyController interface contract

---
+++ backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/HierarchyController.java
@@ -0,0 +1,27 @@
+package dev.werricsson.word_hierarchy_analyzer.controller;
+
+import dev.werricsson.word_hierarchy_analyzer.model.request.HierarchyRequest;
+import dev.werricsson.word_hierarchy_analyzer.model.response.HierarchyResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface HierarchyController {
+
+    @PostMapping
+    ResponseEntity<Void> save(@RequestBody HierarchyRequest request);
+
+    @GetMapping
+    ResponseEntity<List<HierarchyResponse>> findAll();
+
+    @GetMapping("/{id}")
+    ResponseEntity<HierarchyResponse> find(@PathVariable String id);
+
+    @PutMapping("/{id}")
+    ResponseEntity<HierarchyResponse> update(@PathVariable String id, @RequestBody HierarchyRequest request);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> delete(@PathVariable String id);
+
+}
