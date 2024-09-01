1e7428d9ccd63b6982540876dbf5041ceb47b685
Werricsson <werricsson.santos@hotmail.com>
2024-09-01 09:21:30 -0300

    :construction: Create service to populate mongodb with JSON data

---
+++ backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/service/DataImportService.java
@@ -0,0 +1,34 @@
+package dev.werricsson.word_hierarchy_analyzer.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DataImportService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void importData() {
+        try {
+            InputStream inputStream = new ClassPathResource("dicts/hierarchy.json").getInputStream();
+
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, Object>> data = mapper.readValue(inputStream, new TypeReference<List<Map<String, Object>>>() {});
+
+            mongoTemplate.insert(data, "hierarchies");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
