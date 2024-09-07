def4dad2601f1c62a027d0e93e1587ac5e1dfce7
Werricsson <werricsson.santos@hotmail.com>
2024-09-07 19:54:46 -0300

    :recycle: Adapt controller to receive WordAnalysisRequest as a parameter

--- backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/impl/WordAnalyzerControllerImpl.java
+++ backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/impl/WordAnalyzerControllerImpl.java
@@ -1,13 +1,11 @@
 package dev.werricsson.word_hierarchy_analyzer.controller.impl;
 
 import dev.werricsson.word_hierarchy_analyzer.controller.contract.WordAnalyzerController;
+import dev.werricsson.word_hierarchy_analyzer.model.request.WordAnalysisRequest;
 import dev.werricsson.word_hierarchy_analyzer.service.WordAnalyzerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -21,9 +19,9 @@ public class WordAnalyzerControllerImpl implements WordAnalyzerController {
 
     @Override
     @PostMapping("/analyze")
-    public ResponseEntity<Map<String, Integer>> analyze(@RequestBody String text) {
+    public ResponseEntity<Map<String, Integer>> analyze(@RequestBody WordAnalysisRequest request) {
         // Chama o serviço para analisar o texto
-        Map<String, Integer> analysisResult = wordAnalyzerService.analyze(text);
+        Map<String, Integer> analysisResult = wordAnalyzerService.analyze(request.getDepth(), request.getText());
 
         // Retorna a resposta com o resultado da análise
         return ResponseEntity.ok(analysisResult);
