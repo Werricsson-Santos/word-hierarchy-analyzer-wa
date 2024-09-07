0e70aab859e2cc97cbaed8552341f8621d822889
Werricsson <werricsson.santos@hotmail.com>
2024-09-07 19:51:56 -0300

    :recycle: Change contract to post WordAnalysisRequest

--- backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/contract/WordAnalyzerController.java
+++ backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/contract/WordAnalyzerController.java
@@ -1,5 +1,6 @@
 package dev.werricsson.word_hierarchy_analyzer.controller.contract;
 
+import dev.werricsson.word_hierarchy_analyzer.model.request.WordAnalysisRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,5 +9,5 @@ import java.util.Map;
 
 public interface WordAnalyzerController {
     @PostMapping
-    ResponseEntity<Map<String, Integer>> analyze(@RequestBody String text);
+    ResponseEntity<Map<String, Integer>> analyze(@RequestBody WordAnalysisRequest text);
 }
