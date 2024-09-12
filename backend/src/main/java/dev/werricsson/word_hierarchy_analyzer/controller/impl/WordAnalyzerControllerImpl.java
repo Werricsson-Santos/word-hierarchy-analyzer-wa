ed7ac8da484bfefca5c6cf1cbb2b3178c8a86b85
Werricsson <werricsson.santos@hotmail.com>
2024-09-11 22:22:47 -0300

    :bug: Fix CORS issue by allowing requests from the frontend

--- backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/impl/WordAnalyzerControllerImpl.java
+++ backend/src/main/java/dev/werricsson/word_hierarchy_analyzer/controller/impl/WordAnalyzerControllerImpl.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/word-analyzer")
 @AllArgsConstructor
